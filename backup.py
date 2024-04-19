43035c5f9701e31733ff51c3d85ff9e7baca5c6f
EC2 Default User <ec2-user@ip-172-31-46-183.ap-southeast-2.compute.internal>
2024-04-19 05:25:49 +0000

    19 APR

--- backup.py
+++ backup.py
@@ -2,44 +2,81 @@
 
 """
 Author: William Albert Symes
-Email: wasymes@hotmail.com 
+Email: symeswilliam7@gmail.com
 Program: Backup log
 Version: 1.1
+Copyright: All rights Reserved. William Albert Symes
 """
 import sys
 import os
 import pathlib
 import shutil
+import smtplib
 from datetime import datetime
-from backupcfg import jobs, destPath
+from backupcfg import jobs, destPath, logPath, smtp
+
+def Logging(message, dateTimeStamp):# creates log message with date time stamp
+    try:
+        file = open(logPath, "a")
+        file.write(f"{message} {dateTimeStamp}\n")
+        file.close()
+    except FileNotFoundError:
+        print("ERROR: File does not exist")
+    except IOError:
+        print("ERROR: File is not accesible")
+        
+def sendEmail(message, dateTimeStamp): 
+    
+    # create email message 
+    email = 'To: ' + smtp["recipient"] + '\n' + 'From: ' + smtp["sender"] +'\n' + 'Subject: Backup Error\n\n' + message + '\n' 
+    # connect to email server and send email
+    try:
+        smtp_server = smtplib.SMTP(smtp["server"], smtp["port"])# looking for server and port
+        smtp_server.ehlo()
+        smtp_server.starttls()
+        smtp_server.ehlo()
+        smtp_server.login(smtp["user"], smtp["password"])# signing in with the user and password
+        smtp_server.sendmail(smtp["sender"], smtp["recipient"], email)# distingushing between the sender and recipient email
+        smtp_server.close()
+    except Exception as e:
+        print("ERROR: Send email failed: " + str(e), file=sys.stderr)# displays ERROR on screen in sending email.
+        
+def error(errorMessage, dateTimeStamp):
+    print(errorMessage)
+    Logging(f"FAILURE {errorMessage} ", dateTimeStamp)#write failure to log file
+    sendEmail(errorMessage , dateTimeStamp)#email message to admin
+    
+
 
 def main():
+
     dateTimeStamp = datetime.now().strftime("%Y%m%d-%H%M%S")# Makes a date time stamp
     argCount = len(sys.argv)
-    if argCount != 2:# If not equal to 2
-        print("ERROR: Job name is missing from command line")
+    if argCount != 2:# If not equal to 2 and testing job name is specified
+        error("ERROR: Job name is missing from command line", dateTimeStamp)
     else:
         jobname = sys.argv[1]
-        if jobname not in jobs:
-            print("ERROR: jobname is not in jobs")
+        if jobname not in jobs:# testing if job name is vailed
+            error("ERROR: jobname is not in jobs", dateTimeStamp)
         else:
-            srcPath = jobs[jobname]
-            if not os.path.exists(srcPath):
-                print("Error: Source path file does not exist.")
-            else:
-                if not os.path.exists(destPath):
-                    print('Error destination path {destPath} does not exists')
+            for srcPath in jobs[jobname]:
+                if not os.path.exists(srcPath):# testing the path to source
+                    error("ERROR: Source path file does not exist.", dateTimeStamp)
                 else:
-                    
-                    srcDetails = pathlib.PurePath(srcPath)
-                    
-                    dstLoc = destPath + "/" + srcDetails.name + "-" + dateTimeStamp
-                    
-                    if pathlib.Path(srcPath).is_dir():
-                        shutil.copytree(srcPath, dstLoc)
+                    if not os.path.exists(destPath):# testing pasth to destination
+                        error("ERROR: Destination path {destPath} does not exists", dateTimeStamp)
                     else:
-                        shutil.copy2(srcPath, dstLoc)
-            pass
+                        
+                        srcDetails = pathlib.PurePath(srcPath)
+                        
+                        dstLoc = destPath + "/" + srcDetails.name + "-" + dateTimeStamp
+                        
+                        if pathlib.Path(srcPath).is_dir():#copying directory
+                            shutil.copytree(srcPath, dstLoc)
+                        else:
+                            shutil.copy2(srcPath, dstLoc)#copies the file
+                        Logging(f"SUCCESS copied {srcPath} to {dstLoc}", dateTimeStamp)# path success message displayed in backups.log
+
     
 
 if __name__ == '__main__':
--- backupcfg.py
+++ backupcfg.py
@@ -1,4 +1,15 @@
-jobs ={'job17': '/home/ec2-user/environment/ictprg302/file1.dat',
-       'job18': '/home/ec2-user/environment/ictprg302/dict.dat'}
+jobs ={'job17': ['/home/ec2-user/environment/ictprg302/file1.dat'],
+       'job18': ['/home/ec2-user/environment/ictprg302/dict.dat'],
+       'job19': ['/home/ec2-user/environment/ictprg302/file1.dat','/home/ec2-user/environment/ictprg302/file2.dat','/home/ec2-user/environment/ictprg302/file3.dat']}
        
-destPath = '/home/ec2-user/environment/ictprg302/backups'       
+destPath = '/home/ec2-user/environment/ictprg302/backups'  
+
+ # SMTP settings
+smtp = {"sender": "symeswilliam7@gmail.com", # elasticemail.com verified sender
+"recipient": "09135451@students.sunitafe.edu.au", # elasticemail.com verified recipient
+"server": "smtp.elasticemail.com", # elasticemail.com SMTP server
+"port": 2525, # elasticemail.com SMTP port
+"user": "symeswilliam7@gmail.com", # elasticemail.com user
+"password": ""} # elasticemail.com password
+
+logPath = '/home/ec2-user/environment/ictprg302/backups.log'
